c50d27405056ef2004aa54ef1d83866d82efae2f
Ariel Amor <arielamorgarcia@gmail.com>
2020-12-27 12:41:55 +0100

    output files

--- deploy/terraform/main.tf
+++ deploy/terraform/main.tf
@@ -23,7 +23,7 @@ resource "aws_elastic_beanstalk_application" "application" {
 resource "aws_elastic_beanstalk_environment" "environment" {
   name                = "${local.appname}-environment"
   application         = aws_elastic_beanstalk_application.application.name
-  solution_stack_name = "64bit Amazon Linux 2 v3.0.3 running Go 1.15"
+  solution_stack_name = "64bit Amazon Linux 2018.03 v2.17.1 running Go 1.15.4"
 
     setting {
         namespace = "aws:autoscaling:launchconfiguration"
