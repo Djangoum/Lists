5cda0e81f60abe508a882b577accfb104fd52943
Ariel Amor <arielamorgarcia@gmail.com>
2020-12-27 12:31:30 +0100

    new terraform and new outputs

--- deploy/terraform/main.tf
+++ deploy/terraform/main.tf
@@ -6,12 +6,29 @@ terraform {
     }
 }
 
+locals {
+    appversion = "001"
+    appname = "lists-backend-${terraform.workspace}-${local.appversion}"
+}
+
 provider "aws" {
     profile = "default"
     region = "eu-west-3"
 }
 
-resource "aws_elastic_beanstalk_application" "tftest" {
-  name        = "lists-${terraform.workspace}-001"
-  description = "lists backend api"
+resource "aws_elastic_beanstalk_application" "application" {
+  name        = local.appname
 }
+
+resource "aws_elastic_beanstalk_environment" "environment" {
+  name                = "${local.appname}-environment"
+  application         = aws_elastic_beanstalk_application.application.name
+  solution_stack_name = "64bit Amazon Linux 2 v3.0.3 running Go 1.15"
+
+    setting {
+        namespace = "aws:autoscaling:launchconfiguration"
+        name      = "IamInstanceProfile"
+        value     = "aws-elasticbeanstalk-ec2-role"
+    }
+}
+
