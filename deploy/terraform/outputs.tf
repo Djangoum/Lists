066880f604841be341015925f189d2f2f8f2b0fd
Ariel Amor <arielamorgarcia@gmail.com>
2020-12-27 13:33:44 +0100

    new deploy steps

--- deploy/terraform/outputs.tf
+++ deploy/terraform/outputs.tf
@@ -2,6 +2,6 @@ output "app_name" {
     value = aws_elastic_beanstalk_application.application.name
 }
 
-output "app_environment_name" {
+output "environment_name" {
     value = aws_elastic_beanstalk_environment.environment.name
 }
