058e18c84127dddbf8b8eb84ada2370984e51785
Ariel Amor <arielamorgarcia@gmail.com>
2020-12-27 14:45:22 +0100

    new port to listen to

--- main.go
+++ main.go
@@ -8,22 +8,21 @@ import (
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/gorilla/mux"
 
-	"home.com/lists/backend/handlers"
 	"home.com/lists/backend/middlewares"
-	"home.com/lists/backend/repositories"
-	"home.com/lists/backend/usecases/users"
 )
 
 func main() {
-	db := repositories.CreateDatabaseConnection()
-	usersRepo := repositories.NewUsersMysqlRepository(db)
-	usersService := users.NewService(usersRepo)
+	// db := repositories.CreateDatabaseConnection()
+	// usersRepo := repositories.NewUsersMysqlRepository(db)
+	// usersService := users.NewService(usersRepo)
 
 	r := mux.NewRouter()
-	handlers.RegisterListsHandlers(r)
-	handlers.RegisterUserHandlers(r, usersService)
+	// handlers.RegisterListsHandlers(r)
+	// handlers.RegisterUserHandlers(r, usersService)
 
 	httpPipeline := middlewares.RegisterMiddlewares(r)
+
+	r.HandleFunc("/greet", GreeterHandler)
 
 	srv := &http.Server{
 		Handler:      httpPipeline,
@@ -35,4 +34,9 @@ func main() {
 	var err = srv.ListenAndServe()
 
 	stdlog.Fatal(err)
+}
+
+func GreeterHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("hello world"))
+	w.WriteHeader(http.StatusOK)
 }
